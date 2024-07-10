55369b9562c7e247dad4d15327c1086aa4f1e275
Artyom Pervukhin <github@artyom.dev>
2024-07-11 00:07:36 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/artyom/dump-github-issue
+
+go 1.22.0
+
+require github.com/google/go-github/v62 v62.0.0
+
+require github.com/google/go-querystring v1.1.0 // indirect
---
+++ main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+
+	"github.com/google/go-github/v62/github"
+)
+
+func main() {
+	log.SetFlags(0)
+	flag.Parse()
+	if err := run(context.Background(), flag.Arg(0)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context, url string) error {
+	if url == "" {
+		return errors.New("expect github issue url as the first argument")
+	}
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		return errors.New("please set GITHUB_TOKEN env")
+	}
+	p, err := parseUrl(url)
+	if err != nil {
+		return err
+	}
+	client := github.NewClient(nil).WithAuthToken(token)
+	issue, _, err := client.Issues.Get(ctx, p.org, p.repo, p.issueNumber)
+	if err != nil {
+		return err
+	}
+	var out []byte
+	out = append(out, "<github_issue><title>"...)
+	out = append(out, issue.GetTitle()...)
+	out = append(out, "</title><date>"...)
+	out = issue.GetCreatedAt().AppendFormat(out, "Monday, 02 Jan 2006")
+	out = append(out, "</date>\n"...)
+	out = append(out, issue.GetBody()...)
+	out = maybeAddNewline(out)
+	out = append(out, "</github_issue>\n"...)
+
+	opt := &github.IssueListCommentsOptions{ListOptions: github.ListOptions{PerPage: 100}}
+	for {
+		comments, resp, err := client.Issues.ListComments(ctx, p.org, p.repo, p.issueNumber, opt)
+		if err != nil {
+			return err
+		}
+		for _, comment := range comments {
+			out = append(out, "<comment><user>"...)
+			out = append(out, comment.User.GetLogin()...)
+			out = append(out, "</user><date>"...)
+			out = comment.GetCreatedAt().AppendFormat(out, "Monday, 02 Jan 2006")
+			out = append(out, "</date>\n"...)
+			out = append(out, comment.GetBody()...)
+			out = maybeAddNewline(out)
+			out = append(out, "</comment>\n"...)
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		opt.Page = resp.NextPage
+	}
+	_, err = os.Stdout.Write(out)
+	return err
+}
+
+func maybeAddNewline(b []byte) []byte {
+	if len(b) == 0 || b[len(b)-1] == '\n' {
+		return b
+	}
+	return append(b, '\n')
+}
+
+type issueParams struct {
+	org         string
+	repo        string
+	issueNumber int
+}
+
+var githubIssueUrl = regexp.MustCompile(`^https://github\.com/([\w-]+)/([\w-]+)/issues/(\d+)$`)
+
+func parseUrl(s string) (*issueParams, error) {
+	m := githubIssueUrl.FindStringSubmatch(s)
+	if m == nil {
+		return nil, fmt.Errorf("%q does not match %v", s, githubIssueUrl)
+	}
+	n, err := strconv.Atoi(m[3])
+	if err != nil {
+		return nil, err
+	}
+	return &issueParams{
+		org:         m[1],
+		repo:        m[2],
+		issueNumber: n,
+	}, nil
+}
